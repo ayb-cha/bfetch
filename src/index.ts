71ef34e6052da1beebab6eb8cf70746d26b19e8c
Ayoub Chaaoui <ayoubchaaoui02@gmail.com>
2026-02-14 21:55:19 +0000

    refactor: rename n2

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import type { Fetch } from '@/types.ts'
 import { createFetch } from '@/fetch.ts'
 
-export const bee: Fetch = createFetch({})
+export const unfee: Fetch = createFetch({})
 
 export * from '@/types.ts'
