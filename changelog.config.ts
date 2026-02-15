fb6cd4db33eb6cda679cc30d71805d867c371c8f
Ayoub Chaaoui <ayoubchaaoui02@gmail.com>
2026-02-15 11:18:16 +0000

    chore: output change logs

---
+++ changelog.config.ts
@@ -0,0 +1,12 @@
+export default {
+  preset: 'angular',
+  types: [
+    { type: 'feat', section: '✨ Features' },
+    { type: 'fix', section: '🐛 Bug Fixes' },
+    { type: 'chore', section: '🧹 Chores' },
+    { type: 'docs', section: '📚 Documentation' },
+    { type: 'refactor', section: '🛠 Refactors' },
+    { type: 'perf', section: '⚡ Performance' },
+    { type: 'test', section: '✅ Tests' },
+  ],
+}
